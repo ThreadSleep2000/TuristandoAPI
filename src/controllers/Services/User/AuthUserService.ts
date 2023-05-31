59fd3d8910bf6ae1c13ea97eba7a412feb3815a6
Matheus Martins <126476964+ThreadSleep2000@users.noreply.github.com>
2023-05-30 22:45:23 -0300

    Add files via upload

---
+++ src/controllers/Services/User/AuthUserService.ts
@@ -0,0 +1,33 @@
+import prismaClient from "../../../prisma";
+
+interface AuthRequest {
+    email: string;
+    password: string;
+}
+
+class AuthUserService {
+    async execute({ email, password }: AuthRequest) {
+        //verificar se email existe
+        const user = await prismaClient.user.findFirst({
+        where:{
+            email:email
+        }
+        })
+
+        if (!user){
+            throw new Error("User/PassWord Invalido")
+        }
+        //verificar validade password
+    
+        if (user.password!==password){
+            throw new Error("User/PassWord Invalido.") 
+        }
+
+        return {
+            nomeUsuario:user.name
+        }
+    }
+}
+
+export { AuthUserService }
+
